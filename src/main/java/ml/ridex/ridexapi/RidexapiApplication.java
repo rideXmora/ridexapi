28044b9480ee05235cd2615544737dab6e302d2a
kavinda <kavindar.18@cse.mrt.ac.lk>
2021-08-07 22:38:11 +0530

    first commit

---
+++ src/main/java/ml/ridex/ridexapi/RidexapiApplication.java
@@ -0,0 +1,13 @@
+package ml.ridex.ridexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidexapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidexapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/ridex/ridexapi/RidexapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.ridex.ridexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidexapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
