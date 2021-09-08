91182fb5b4ae9b163479dd08d65de2425fec2464
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-08 12:09:48 +0530

    Custom exeption

--- src/main/java/ml/ridex/ridexapi/exception/InvalidJwtAuthenticationException.java
+++ src/main/java/ml/ridex/ridexapi/exception/InvalidJwtAuthenticationException.java
@@ -1,2 +1,9 @@
-package ml.ridex.ridexapi.exception;public class InvalidJwtAuthenticationException {
+package ml.ridex.ridexapi.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+    public InvalidJwtAuthenticationException(String e) {
+        super(e);
+    }
 }
