2ae3e2d12eda3d4ecc474bbf17a0f8bfdeffee77
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-09 01:56:51 +0530

    Test controller

--- src/main/java/ml/ridex/ridexapi/controller/AuthController.java
+++ src/main/java/ml/ridex/ridexapi/controller/AuthController.java
@@ -1,2 +1,16 @@
-package ml.ridex.ridexapi.controller;public class AuthController {
+package ml.ridex.ridexapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping("passenger/login")
+    public String passengerLogin() {
+        System.out.println("It's working");
+        return "Tada";
+    }
 }
