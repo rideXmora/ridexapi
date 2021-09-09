ff4169b7728cb559a6275a586fd8c3e29c430794
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-10 01:05:30 +0530

    Add model mapper config

--- src/main/java/ml/ridex/ridexapi/config/ModelMapperConfigurer.java
+++ src/main/java/ml/ridex/ridexapi/config/ModelMapperConfigurer.java
@@ -1,2 +1,13 @@
-package ml.ridex.ridexapi.config;public class ModelMapperConfigurer {
+package ml.ridex.ridexapi.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfigurer {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
