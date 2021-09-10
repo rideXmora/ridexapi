8c6d2f55a8fcedbf8a86a6f5967a0d46daed4d75
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-11 00:32:22 +0530

    Test otp gen class

--- src/test/java/ml/ridex/ridexapi/helper/OtpGeneratorTest.java
+++ src/test/java/ml/ridex/ridexapi/helper/OtpGeneratorTest.java
@@ -1,2 +1,27 @@
-package ml.ridex.ridexapi.helper;public class OtpGeneratorTest {
+package ml.ridex.ridexapi.helper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class OtpGeneratorTest {
+    private OtpGenerator otpGenerator;
+
+    @BeforeEach
+    void setup() throws NoSuchAlgorithmException {
+        otpGenerator = new OtpGenerator();
+    }
+
+    @Test
+    public void getOtp() throws InvalidKeyException {
+        String otp = otpGenerator.generateOTP();
+        assertThat(otp).asString();
+    }
 }
