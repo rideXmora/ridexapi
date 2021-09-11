8e9471254be0f5285297350ded8ef54d5e72ee4b
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-11 08:59:44 +0530

    Add JWT token to the DTO

--- src/main/java/ml/ridex/ridexapi/model/dto/PassengerVerifiedResDTO.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/PassengerVerifiedResDTO.java
@@ -1,2 +1,33 @@
-package ml.ridex.ridexapi.model.dto;public class PassengerVerifiedResDTO {
+package ml.ridex.ridexapi.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class PassengerVerifiedResDTO {
+    private String id;
+
+    private String phone;
+
+    private String email;
+
+    private String name;
+
+    private Integer totalRating;
+
+    private Integer totalRides;
+
+    private List<String> pastRides;
+
+    private String token;
+
+    private UUID refreshToken;
+
+    private Boolean enabled;
+
+    private Boolean suspend;
 }
