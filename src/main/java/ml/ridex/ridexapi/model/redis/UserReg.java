e97c7a5e73796cb2ba3f6b19027034a1b5130b0a
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-17 20:57:38 +0530

    User registration redis

--- src/main/java/ml/ridex/ridexapi/model/redis/UserReg.java
+++ src/main/java/ml/ridex/ridexapi/model/redis/UserReg.java
@@ -1,2 +1,17 @@
-package ml.ridex.ridexapi.model.redis;public class UserReg {
+package ml.ridex.ridexapi.model.redis;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@RedisHash("UserReg")
+public class UserReg {
+    @Id
+    private String phone;
+    private String otp;
 }
