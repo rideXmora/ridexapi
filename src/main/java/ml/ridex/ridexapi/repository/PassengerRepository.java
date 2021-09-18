cbe3678737b2b284522339b16e875985cb248623
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-18 13:07:30 +0530

    Add methods to check the exsistance

--- src/main/java/ml/ridex/ridexapi/repository/PassengerRepository.java
+++ src/main/java/ml/ridex/ridexapi/repository/PassengerRepository.java
@@ -4,5 +4,6 @@ import ml.ridex.ridexapi.model.dao.Passenger;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PassengerRepository extends MongoRepository<Passenger, String> {
-    public Passenger findByPhoneAndOtp(String phone, String otp);
+    public Passenger findByPhone(String phone);
+    public Boolean existsByPhone(String phone);
 }
