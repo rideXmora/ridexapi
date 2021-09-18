3a9e27bf21f4ae72100d056a0fb90a1d978b5f29
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-18 07:07:32 +0530

    Change Otp generator

--- src/main/java/ml/ridex/ridexapi/helper/OtpGenerator.java
+++ src/main/java/ml/ridex/ridexapi/helper/OtpGenerator.java
@@ -1,6 +1,9 @@
 package ml.ridex.ridexapi.helper;
 
 import com.eatthepath.otp.TimeBasedOneTimePasswordGenerator;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.KeyGenerator;
@@ -8,12 +11,15 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
+import java.util.Date;
 
 @Component
 public class OtpGenerator {
     private final TimeBasedOneTimePasswordGenerator totp;
     private final KeyGenerator keyGenerator;
     private final Key key;
+    @Getter
+    private long exp;
 
     public OtpGenerator() throws NoSuchAlgorithmException {
         this.totp = new TimeBasedOneTimePasswordGenerator();
@@ -22,8 +28,8 @@ public class OtpGenerator {
         this.key = keyGenerator.generateKey();
     }
 
-    public String generateOTP() throws InvalidKeyException {
+    public Otp generateOTP() throws InvalidKeyException {
         final Instant now = Instant.now();
-        return totp.generateOneTimePasswordString(key, now);
+        return new Otp(totp.generateOneTimePasswordString(key, now), now.getEpochSecond() + 300000);
     }
 }
