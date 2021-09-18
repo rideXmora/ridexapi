ca75b801693cf94846bafa38f6e905da471961e1
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-18 13:08:53 +0530

    Add validation

--- src/main/java/ml/ridex/ridexapi/model/dto/PhoneAuthDTO.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/PhoneAuthDTO.java
@@ -3,11 +3,13 @@ package ml.ridex.ridexapi.model.dto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Data
 @RequiredArgsConstructor
 public class PhoneAuthDTO {
+    @NotBlank
     @Pattern(regexp="(\\+94\\d{9})")
     private String phone;
 }
