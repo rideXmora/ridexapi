141cde737707d76d28a9908b5e2bd1e13b12eaa7
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-18 12:24:27 +0530

    Add global error handling

--- src/main/java/ml/ridex/ridexapi/controller/ExceptionController.java
+++ src/main/java/ml/ridex/ridexapi/controller/ExceptionController.java
@@ -1,2 +1,26 @@
-package ml.ridex.ridexapi.controller;public class ExceptionController {
+package ml.ridex.ridexapi.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionController {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 }
