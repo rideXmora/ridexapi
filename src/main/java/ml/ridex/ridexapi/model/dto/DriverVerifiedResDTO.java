52b253d47f1f260fcd607c876d07349fc5227af8
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-21 01:10:15 +0530

    Add Driver dto

--- src/main/java/ml/ridex/ridexapi/model/dto/DriverVerifiedResDTO.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/DriverVerifiedResDTO.java
@@ -1,2 +1,36 @@
-package ml.ridex.ridexapi.model.dto;public class DriverVerifiedResDTO {
+package ml.ridex.ridexapi.model.dto;
+
+import lombok.Data;
+import ml.ridex.ridexapi.model.daoHelper.DriverOrganization;
+import ml.ridex.ridexapi.model.daoHelper.Vehicle;
+
+import java.util.List;
+
+@Data
+public class DriverVerifiedResDTO {
+    private String id;
+
+    private String phone;
+
+    private String token;
+
+    private String refreshToken;
+
+    private String email;
+
+    private String name;
+
+    private Integer totalRating;
+
+    private Integer totalRides;
+
+    private List<String> pastRides;
+
+    private Vehicle vehicle;
+
+    private DriverOrganization driverOrganization;
+
+    private Boolean enabled;
+
+    private Boolean suspend;
 }
