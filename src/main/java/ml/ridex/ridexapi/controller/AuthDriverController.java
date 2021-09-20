785e9fe37ddd448eb7394c75e913effd97eedec6
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-21 01:11:47 +0530

    Login controller passenger, driver

--- src/main/java/ml/ridex/ridexapi/controller/AuthDriverController.java
+++ src/main/java/ml/ridex/ridexapi/controller/AuthDriverController.java
@@ -8,8 +8,8 @@ import ml.ridex.ridexapi.enums.Role;
 import ml.ridex.ridexapi.exception.EntityNotFoundException;
 import ml.ridex.ridexapi.exception.InvalidOperationException;
 import ml.ridex.ridexapi.model.dao.Driver;
+import ml.ridex.ridexapi.model.dto.DriverVerifiedResDTO;
 import ml.ridex.ridexapi.model.dto.OtpVerifyDTO;
-import ml.ridex.ridexapi.model.dto.PassengerVerifiedResDTO;
 import ml.ridex.ridexapi.model.dto.PhoneAuthDTO;
 import ml.ridex.ridexapi.service.AuthService;
 import org.modelmapper.ModelMapper;
@@ -33,8 +33,11 @@ public class AuthDriverController {
 
         @PostMapping("/phoneAuth")
         @ResponseStatus(HttpStatus.OK)
-        @Operation(summary = "Passenger Send OTP")
-        @ApiResponse(responseCode = "200", description = "Saved user in memory")
+        @Operation(summary = "Driver Send OTP")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "Saved user in memory"),
+                @ApiResponse(responseCode = "400", description = "Driver already exits")
+        })
         public String driverPhoneAuth(@Valid @RequestBody PhoneAuthDTO phoneAuthDTO) {
                 try {
                         return authService.driverPhoneAuth(phoneAuthDTO);
@@ -52,11 +55,11 @@ public class AuthDriverController {
                 @ApiResponse(responseCode = "201", description = "Successful"),
                 @ApiResponse(responseCode = "400", description = "Invalid OTP/phone")
         })
-        public PassengerVerifiedResDTO driverVerify(@Valid @RequestBody OtpVerifyDTO data) {
+        public DriverVerifiedResDTO driverVerify(@Valid @RequestBody OtpVerifyDTO data) {
                 try {
                         Driver driver = authService.driverVerify(data);
                         String token = authService.createJwtToken(data.getPhone(), Role.DRIVER);
-                        PassengerVerifiedResDTO responseDTO = modelMapper.map(driver, PassengerVerifiedResDTO.class);
+                        DriverVerifiedResDTO responseDTO = modelMapper.map(driver, DriverVerifiedResDTO.class);
                         responseDTO.setToken(token);
                         return responseDTO;
                 }
@@ -65,6 +68,42 @@ public class AuthDriverController {
                 }
                 catch (EntityNotFoundException e) {
                         throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+                }
+        }
+
+        @PostMapping("/loginAuth")
+        @ResponseStatus(HttpStatus.OK)
+        @Operation(summary = "Driver/login Send OTP")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "Saved user in memory"),
+                @ApiResponse(responseCode = "400", description = "Driver already exits")
+        })
+        public String driverLoginAuth(@Valid @RequestBody PhoneAuthDTO phoneAuthDTO) {
+                try {
+                        return authService.driverLoginAuth(phoneAuthDTO);
+                } catch(InvalidOperationException e) {
+                        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+                } catch (InvalidKeyException e) {
+                        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+                }
+        }
+
+        @PostMapping("/loginVerify")
+        @ResponseStatus(HttpStatus.CREATED)
+        @Operation(summary = "OTP verification")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "201", description = "Login successful"),
+                @ApiResponse(responseCode = "400", description = "Invalid OTP/phone")
+        })
+        public DriverVerifiedResDTO driverLoginVerify(@Valid @RequestBody OtpVerifyDTO data) {
+                try {
+                        Driver driver = authService.driverLoginVerify(data);
+                        String token = authService.createJwtToken(data.getPhone(), Role.DRIVER);
+                        DriverVerifiedResDTO responseDTO = modelMapper.map(driver, DriverVerifiedResDTO.class);
+                        responseDTO.setToken(token);
+                        return responseDTO;
+                } catch(InvalidOperationException e) {
+                        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
                 }
         }
 }
--- src/main/java/ml/ridex/ridexapi/controller/AuthPassengerController.java
+++ src/main/java/ml/ridex/ridexapi/controller/AuthPassengerController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import ml.ridex.ridexapi.enums.Role;
 import ml.ridex.ridexapi.exception.EntityNotFoundException;
 import ml.ridex.ridexapi.exception.InvalidOperationException;
-import ml.ridex.ridexapi.model.dao.Driver;
 import ml.ridex.ridexapi.model.dao.Passenger;
 import ml.ridex.ridexapi.model.dto.OtpVerifyDTO;
 import ml.ridex.ridexapi.model.dto.PassengerVerifiedResDTO;
@@ -21,7 +20,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.InvalidKeyException;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth/passenger")
@@ -35,8 +33,11 @@ public class AuthPassengerController {
 
     @PostMapping("/phoneAuth")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Passenger Send OTP")
-    @ApiResponse(responseCode = "200", description = "Saved user in memory")
+    @Operation(summary = "Passenger/signup Send OTP")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Saved user in memory"),
+            @ApiResponse(responseCode = "400", description = "Passenger already exits")
+    })
     public String passengerPhoneAuth(@Valid @RequestBody PhoneAuthDTO phoneAuthDTO) {
         try {
             return authService.passengerPhoneAuth(phoneAuthDTO);
@@ -51,7 +52,7 @@ public class AuthPassengerController {
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "OTP verification")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Successful"),
+            @ApiResponse(responseCode = "201", description = "Signup successful"),
             @ApiResponse(responseCode = "400", description = "Invalid OTP/phone")
     })
     public PassengerVerifiedResDTO passengerVerify(@Valid @RequestBody OtpVerifyDTO data) {
@@ -70,4 +71,39 @@ public class AuthPassengerController {
         }
     }
 
+    @PostMapping("/loginAuth")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Passenger/login Send OTP")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Saved user in memory"),
+            @ApiResponse(responseCode = "400", description = "Passenger already exits")
+    })
+    public String passengerLoginAuth(@Valid @RequestBody PhoneAuthDTO phoneAuthDTO) {
+        try {
+            return authService.passengerLoginAuth(phoneAuthDTO);
+        } catch(InvalidOperationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (InvalidKeyException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    @PostMapping("/loginVerify")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "OTP verification")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Login successful"),
+            @ApiResponse(responseCode = "400", description = "Invalid OTP/phone")
+    })
+    public PassengerVerifiedResDTO passengerLoginVerify(@Valid @RequestBody OtpVerifyDTO data) {
+        try {
+            Passenger passenger = authService.passengerLoginVerify(data);
+            String token = authService.createJwtToken(data.getPhone(), Role.PASSENGER);
+            PassengerVerifiedResDTO responseDTO = modelMapper.map(passenger, PassengerVerifiedResDTO.class);
+            responseDTO.setToken(token);
+            return responseDTO;
+        } catch(InvalidOperationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
 }
