4e7c8f8e61f81d83768ae431c87aa3ad772d3cfd
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-23 02:12:46 +0530

    Sendgrid config

--- src/main/java/ml/ridex/ridexapi/config/SendGridConfig.java
+++ src/main/java/ml/ridex/ridexapi/config/SendGridConfig.java
@@ -1,2 +1,23 @@
-package ml.ridex.ridexapi.config;public class SendGridConfig {
+package ml.ridex.ridexapi.config;
+
+import com.sendgrid.SendGrid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SendGridConfig {
+    @Value("${EMAIL_API_KEY}")
+    private String sendGridAPIKey;
+
+    private static SendGrid sendGrid;
+
+    @Bean
+    public SendGrid getSendGridClient() {
+        if (sendGrid != null) {
+            return sendGrid;
+        }
+        sendGrid = new SendGrid(sendGridAPIKey);
+        return sendGrid;
+    }
 }
