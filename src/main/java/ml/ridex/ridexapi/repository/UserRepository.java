2a7003f0adada0e8415c070b10d30eec80ea8738
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-24 16:47:25 +0530

    Add findByPhone to userRepo

--- src/main/java/ml/ridex/ridexapi/repository/UserRepository.java
+++ src/main/java/ml/ridex/ridexapi/repository/UserRepository.java
@@ -3,6 +3,8 @@ package ml.ridex.ridexapi.repository;
 import ml.ridex.ridexapi.model.dao.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface userRepository extends MongoRepository<User, String> {
+import java.util.Optional;
 
+public interface UserRepository extends MongoRepository<User, String> {
+    public Optional<User> findByPhone(String phone);
 }
