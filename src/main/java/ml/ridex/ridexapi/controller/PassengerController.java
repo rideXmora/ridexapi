6e38634942804190e620beb58f7aa079d236f582
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-25 11:49:13 +0530

    Minor changes

--- src/main/java/ml/ridex/ridexapi/controller/PassengerController.java
+++ src/main/java/ml/ridex/ridexapi/controller/PassengerController.java
@@ -1,6 +1,5 @@
 package ml.ridex.ridexapi.controller;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @RestController
-@PreAuthorize("hasAuthority('DRIVER')")
+@PreAuthorize("hasAuthority('PASSENGER')")
 @RequestMapping("/api/passenger")
 public class PassengerController {
     @GetMapping
