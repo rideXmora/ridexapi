8caea4b595d167cdc50900edebf6628b02b443c8
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-26 11:29:21 +0530

    Add profileCompleteDTO

--- src/main/java/ml/ridex/ridexapi/model/dto/PassengerProfileComplete.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/PassengerProfileComplete.java
@@ -1,2 +1,16 @@
-package ml.ridex.ridexapi.model.dto;public class PassengerProfileComplete {
+package ml.ridex.ridexapi.model.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class PassengerProfileComplete {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String name;
 }
