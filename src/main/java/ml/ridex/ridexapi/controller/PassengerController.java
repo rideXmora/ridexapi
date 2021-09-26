919a4ca78a4819ea89039ec99e20c3cf180c8150
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-26 11:28:36 +0530

    Add passenger controller and service

--- src/main/java/ml/ridex/ridexapi/controller/PassengerController.java
+++ src/main/java/ml/ridex/ridexapi/controller/PassengerController.java
@@ -1,19 +1,51 @@
 package ml.ridex.ridexapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import ml.ridex.ridexapi.exception.EntityNotFoundException;
+import ml.ridex.ridexapi.exception.InvalidOperationException;
+import ml.ridex.ridexapi.model.dao.Passenger;
+import ml.ridex.ridexapi.model.dto.PassengerDTO;
+import ml.ridex.ridexapi.model.dto.PassengerProfileComplete;
+import ml.ridex.ridexapi.service.PassengerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @RestController
 @PreAuthorize("hasAuthority('PASSENGER')")
 @RequestMapping("/api/passenger")
 public class PassengerController {
-    @GetMapping
-    public String test(Principal principal) {
-        System.out.println(principal.getName());
-        return "Authed";
+
+    @Autowired
+    private PassengerService passengerService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping("/profileComplete")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Passenger profile complete/ active the account")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Profile completed"),
+            @ApiResponse(responseCode = "400", description = "Database error"),
+            @ApiResponse(responseCode = "401", description = "User is suspended")
+    })
+    public PassengerDTO profileComplete(@Valid @RequestBody PassengerProfileComplete data, Principal principal) {
+        try {
+            Passenger passenger = passengerService.profileComplete(principal.getName(), data.getName(), data.getEmail());
+            return modelMapper.map(passenger, PassengerDTO.class);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        } catch (InvalidOperationException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
     }
 }
--- src/main/java/ml/ridex/ridexapi/service/PassengerService.java
+++ src/main/java/ml/ridex/ridexapi/service/PassengerService.java
@@ -1,2 +1,29 @@
-package ml.ridex.ridexapi.service;public class PassengerService {
+package ml.ridex.ridexapi.service;
+
+import ml.ridex.ridexapi.exception.EntityNotFoundException;
+import ml.ridex.ridexapi.exception.InvalidOperationException;
+import ml.ridex.ridexapi.model.dao.Passenger;
+import ml.ridex.ridexapi.repository.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PassengerService {
+    @Autowired
+    private PassengerRepository passengerRepository;
+
+    public Passenger profileComplete(String phone, String email, String name) {
+        Optional<Passenger> passengerOptional = passengerRepository.findByPhone(phone);
+        if(passengerOptional.isEmpty())
+            throw new EntityNotFoundException("User not found");
+        Passenger passenger = passengerOptional.get();
+        if(passenger.getSuspend())
+            throw new InvalidOperationException("User is suspended");
+        passenger.setEmail(email);
+        passenger.setName(name);
+        passenger.setEnabled(true);
+        return passengerRepository.save(passenger);
+    }
 }
