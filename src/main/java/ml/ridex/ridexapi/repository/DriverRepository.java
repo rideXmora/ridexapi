8d9af38ab5bd4499c7f24cb9f1367205e6b5c5f9
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-26 19:40:04 +0530

    Add new methods

--- src/main/java/ml/ridex/ridexapi/repository/DriverRepository.java
+++ src/main/java/ml/ridex/ridexapi/repository/DriverRepository.java
@@ -2,11 +2,19 @@ package ml.ridex.ridexapi.repository;
 
 import ml.ridex.ridexapi.model.dao.Driver;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DriverRepository extends MongoRepository<Driver, String> {
     public Optional<Driver> findByPhone(String phone);
     public Optional<Driver> findByPhoneAndSuspend(String phone, boolean suspend);
     public Boolean existsByPhone(String phone);
+
+    @Query("{ 'driverOrganization._id': ObjectId(?0), 'enabled': ?1 }")
+    public List<Driver> findByDriverOrganizationId(String orgId, boolean enabled);
+
+    @Query("{ 'phone': ?0, 'driverOrganization._id': ObjectId(?1) }")
+    public Optional<Driver> findByPhoneAndDriverOrganizationId(String phone, String orgId);
 }
