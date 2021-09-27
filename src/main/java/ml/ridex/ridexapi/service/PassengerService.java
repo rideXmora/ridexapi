7501d1212fbc1183f8b7c24dd4dd61ca20192445
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-27 09:17:47 +0530

    Passenger profile update

--- src/main/java/ml/ridex/ridexapi/service/PassengerService.java
+++ src/main/java/ml/ridex/ridexapi/service/PassengerService.java
@@ -14,16 +14,29 @@ public class PassengerService {
     @Autowired
     private PassengerRepository passengerRepository;
 
-    public Passenger profileComplete(String phone, String email, String name) {
+    private Passenger getPassenger(String phone) {
         Optional<Passenger> passengerOptional = passengerRepository.findByPhone(phone);
         if(passengerOptional.isEmpty())
             throw new EntityNotFoundException("User not found");
-        Passenger passenger = passengerOptional.get();
+        return passengerOptional.get();
+    }
+
+    public Passenger profileComplete(String phone, String email, String name) throws EntityNotFoundException {
+        Passenger passenger = this.getPassenger(phone);
         if(passenger.getSuspend())
             throw new InvalidOperationException("User is suspended");
         passenger.setEmail(email);
         passenger.setName(name);
         passenger.setEnabled(true);
+        return passengerRepository.save(passenger);
+    }
+
+    public Passenger updateProfile(String phone, String name, String email) throws EntityNotFoundException {
+        Passenger passenger = this.getPassenger(phone);
+        if(passenger.getSuspend())
+            throw new InvalidOperationException("User is suspended");
+        passenger.setName(name);
+        passenger.setEmail(email);
         return passengerRepository.save(passenger);
     }
 }
