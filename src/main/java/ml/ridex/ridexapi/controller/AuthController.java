6a33fd6a42c37d0b4e17a4103edd7da60df66b62
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-28 13:24:33 +0530

    Add messageDTO

--- src/main/java/ml/ridex/ridexapi/controller/AuthController.java
+++ src/main/java/ml/ridex/ridexapi/controller/AuthController.java
@@ -7,13 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import ml.ridex.ridexapi.enums.Role;
 import ml.ridex.ridexapi.exception.EntityNotFoundException;
 import ml.ridex.ridexapi.exception.InvalidOperationException;
-import ml.ridex.ridexapi.model.dto.DriverVerifiedResDTO;
-import ml.ridex.ridexapi.model.dto.OtpVerifyDTO;
-import ml.ridex.ridexapi.model.dto.PassengerVerifiedResDTO;
-import ml.ridex.ridexapi.model.dto.PhoneAuthDTO;
-import ml.ridex.ridexapi.model.dto.AdminDTO;
-import ml.ridex.ridexapi.model.dto.AdminLoginResDTO;
-import ml.ridex.ridexapi.model.dto.OrgAdminLoginResDTO;
+import ml.ridex.ridexapi.model.dto.*;
 import ml.ridex.ridexapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -44,9 +38,9 @@ public class AuthController {
             @ApiResponse(responseCode = "200", description = "Saved user in memory"),
             @ApiResponse(responseCode = "500", description = "Unable to generate OTP")
     })
-    public String passengerPhoneAuth(@Valid @RequestBody PhoneAuthDTO data) {
+    public MessageDTO passengerPhoneAuth(@Valid @RequestBody PhoneAuthDTO data) {
         try {
-            return userService.sendOTP(data.getPhone(), Role.PASSENGER);
+            return new MessageDTO(userService.sendOTP(data.getPhone(), Role.PASSENGER));
         } catch (InvalidKeyException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
