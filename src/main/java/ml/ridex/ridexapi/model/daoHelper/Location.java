b7c8cfa7501e8dc7c1d579c46efe2d6f6047f09e
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-28 21:56:09 +0530

    Add allargCon

--- src/main/java/ml/ridex/ridexapi/model/daoHelper/Location.java
+++ src/main/java/ml/ridex/ridexapi/model/daoHelper/Location.java
@@ -1,9 +1,11 @@
 package ml.ridex.ridexapi.model.daoHelper;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Location {
-    private Float x;
-    private Float y;
+    private Double x;
+    private Double y;
 }
