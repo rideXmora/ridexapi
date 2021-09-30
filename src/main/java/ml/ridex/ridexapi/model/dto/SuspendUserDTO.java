66a01d134c19b6ef886a993a1b438e3a1acf13c5
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-30 11:48:26 +0530

    Add DTOs

---
+++ src/main/java/ml/ridex/ridexapi/model/dto/SuspendUserDTO.java
@@ -0,0 +1,16 @@
+package ml.ridex.ridexapi.model.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class SuspendUserDTO {
+    @NotBlank
+    @Pattern(regexp="(\\+94\\d{9})")
+    private String phone;
+    @NotNull
+    private Boolean suspend;
+}
