13e290a6f4e3b564ed5513fb2dc22a00352d75c8
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-09-30 21:53:39 +0530

    Add Ride finish Dto

--- src/main/java/ml/ridex/ridexapi/model/dto/DriverRideFinishDTO.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/DriverRideFinishDTO.java
@@ -1,2 +1,18 @@
-package ml.ridex.ridexapi.model.dto;public class DriverRideFinishDTO {
+package ml.ridex.ridexapi.model.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class DriverRideFinishDTO {
+    @NotBlank
+    private String id;
+
+    private String driverFeedback;
+    @Min(0)
+    @Max(5)
+    private Byte passengerRating;
 }
