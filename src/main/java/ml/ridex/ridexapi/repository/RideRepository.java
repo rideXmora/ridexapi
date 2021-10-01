8b3b9973ed3480b72e472c0e2c3b32d32defe578
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-10-01 23:20:18 +0530

    Add custom querying methods

--- src/main/java/ml/ridex/ridexapi/repository/RideRepository.java
+++ src/main/java/ml/ridex/ridexapi/repository/RideRepository.java
@@ -1,9 +1,11 @@
 package ml.ridex.ridexapi.repository;
 
+import ml.ridex.ridexapi.enums.RideStatus;
 import ml.ridex.ridexapi.model.dao.Ride;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RideRepository extends MongoRepository<Ride, String> {
@@ -12,4 +14,13 @@ public interface RideRepository extends MongoRepository<Ride, String> {
 
     @Query("{ '_id': ObjectId(?0), 'rideRequest.passenger.phone': ?1 }")
     public Optional<Ride> findByIdAndRideRequestPassengerPhone(String id, String phone);
+
+    @Query("{ 'rideStatus': ?1, 'rideRequest.passenger.phone': ?0 }")
+    public List<Ride> findByRideStatusAndRideRequestPassengerPhone(String phone, RideStatus rideStatus);
+
+    @Query("{ 'rideRequest.organization.id': ?0 }")
+    public List<Ride> findByRideRequestOrganizationId(String id);
+
+    @Query("{ 'rideStatus': ?1, 'rideRequest.driver.phone': ?0 }")
+    public List<Ride> findByRideRequestAndRideRequestDriverPhone(String id, RideStatus rideStatus);
 }
