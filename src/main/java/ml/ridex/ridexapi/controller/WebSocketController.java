5451eafbb988ae858fa885f1658085b0b8319599
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-10-15 18:45:52 +0530

    WS controller

--- src/main/java/ml/ridex/ridexapi/controller/WebSocketController.java
+++ src/main/java/ml/ridex/ridexapi/controller/WebSocketController.java
@@ -1,2 +1,15 @@
-package ml.ridex.ridexapi.controller;public class WebSocketController {
+package ml.ridex.ridexapi.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+    @MessageMapping("/request")
+    @SendTo("/ride/request")
+    public String broadcastRequest(String message) {
+        return message;
+    }
 }
