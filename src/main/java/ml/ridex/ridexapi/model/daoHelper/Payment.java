e612825e30ded1dcc5c1fab731bc4a18f27cddd6
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-10-23 14:04:49 +0530

    Change DTOs

--- src/main/java/ml/ridex/ridexapi/model/daoHelper/Payment.java
+++ src/main/java/ml/ridex/ridexapi/model/daoHelper/Payment.java
@@ -2,9 +2,11 @@ package ml.ridex.ridexapi.model.daoHelper;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ml.ridex.ridexapi.enums.VehicleType;
 
 @Data
 public class Payment {
+    private VehicleType vehicleType;
     private Double ratePerMeter;
     private Double rateWaitingPerMin;
     private Double discount;
--- src/main/java/ml/ridex/ridexapi/model/dto/OrgAdminPaymentDTO.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/OrgAdminPaymentDTO.java
@@ -1,9 +1,14 @@
 package ml.ridex.ridexapi.model.dto;
 
 import lombok.Data;
+import ml.ridex.ridexapi.enums.VehicleType;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 public class OrgAdminPaymentDTO {
+    @NotNull
+    private VehicleType vehicleType;
     private Double ratePerMeter;
     private Double rateWaitingPerMin;
     private Double discount;
