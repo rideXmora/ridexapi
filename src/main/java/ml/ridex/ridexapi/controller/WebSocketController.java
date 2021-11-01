207788edff01e33b0cfa7e0f530ab67aaa1d4410
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-11-01 09:54:48 +0530

    Tested changes

--- src/main/java/ml/ridex/ridexapi/controller/WebSocketController.java
+++ src/main/java/ml/ridex/ridexapi/controller/WebSocketController.java
@@ -1,16 +1,28 @@
 package ml.ridex.ridexapi.controller;
 
+import ml.ridex.ridexapi.model.daoHelper.Location;
+import ml.ridex.ridexapi.model.dto.WSLocation;
 import ml.ridex.ridexapi.model.dto.WSMessageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
-    @MessageMapping("/request")
-    @SendTo("/ride/request")
-    public WSMessageDTO broadcastRequest(WSMessageDTO message) {
-        return message;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+//    @Autowired
+//    private ChatMessageService chatMessageService;
+//    @Autowired
+//    private ChatRoomService chatRoomService;
+
+    @MessageMapping("/chat")
+    public void processMessage(@Payload WSLocation wsLocation) {
+        messagingTemplate.convertAndSendToUser(
+                wsLocation.getReceiverPhone(),"/queue/messages",
+                wsLocation);
     }
 }
