182a4271ed391d0c520782b868ba5f8fab5e1a9d
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-11-03 11:32:21 +0530

    Add arg and noarg contructors

--- src/main/java/ml/ridex/ridexapi/model/dto/SubscriptionRequestDTO.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/SubscriptionRequestDTO.java
@@ -1,10 +1,14 @@
 package ml.ridex.ridexapi.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubscriptionRequestDTO {
 
     private String topicName;
