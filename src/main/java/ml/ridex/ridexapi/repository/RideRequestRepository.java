8d949377696aca46d609da0b95cac9994562c93a
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-11-04 09:06:37 +0530

    Mongorepo method to get rideRequest'

--- src/main/java/ml/ridex/ridexapi/repository/RideRequestRepository.java
+++ src/main/java/ml/ridex/ridexapi/repository/RideRequestRepository.java
@@ -3,5 +3,8 @@ package ml.ridex.ridexapi.repository;
 import ml.ridex.ridexapi.model.dao.RideRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface RideRequestRepository extends MongoRepository<RideRequest, String> {
+    public Optional<RideRequest> findByIdAndPhone(String id, String phone);
 }
