47260cff60e9f9e8dd3f92d85a216a9651b32156
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-11-06 16:44:16 +0530

    Add new OrgAdminDTO

---
+++ src/main/java/ml/ridex/ridexapi/model/dto/OrgAdminDTO.java
@@ -0,0 +1,29 @@
+package ml.ridex.ridexapi.model.dto;
+
+import lombok.Data;
+import ml.ridex.ridexapi.model.daoHelper.Payment;
+
+import java.util.List;
+
+@Data
+public class OrgAdminDTO {
+    private String id;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String businessRegNo;
+
+    private String basedCity;
+
+    private String address;
+
+    private List<Payment> payments;
+
+    private Double totalIncome;
+
+    private Boolean enabled;
+}
