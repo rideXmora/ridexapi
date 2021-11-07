b7d7ee488d7ae34b43db2d2ede50ea5423bd06a3
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-11-08 02:29:54 +0530

    Add vehicleType

--- src/main/java/ml/ridex/ridexapi/model/dto/RideRequestDTO.java
+++ src/main/java/ml/ridex/ridexapi/model/dto/RideRequestDTO.java
@@ -1,11 +1,19 @@
 package ml.ridex.ridexapi.model.dto;
 
 import lombok.Data;
+import ml.ridex.ridexapi.enums.VehicleType;
 import ml.ridex.ridexapi.model.daoHelper.Location;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 public class RideRequestDTO {
+    @NotNull
     private Location startLocation;
+    @NotNull
     private Location endLocation;
+    @NotNull
     private Integer distance;
+    @NotNull
+    private VehicleType vehicleType;
 }
