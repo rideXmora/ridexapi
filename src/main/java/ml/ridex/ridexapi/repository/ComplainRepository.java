31e5e2610929eea86e4b6262608331bb248ff659
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-11-07 10:14:20 +0530

    Add method to get complains

--- src/main/java/ml/ridex/ridexapi/repository/ComplainRepository.java
+++ src/main/java/ml/ridex/ridexapi/repository/ComplainRepository.java
@@ -1,8 +1,12 @@
 package ml.ridex.ridexapi.repository;
 
+import ml.ridex.ridexapi.enums.ComplainStatus;
 import ml.ridex.ridexapi.model.dao.Complain;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface ComplainRepository extends MongoRepository<Complain, String> {
-
+    public List<Complain> findByRideRideRequestOrganizationId(String id);
 }
