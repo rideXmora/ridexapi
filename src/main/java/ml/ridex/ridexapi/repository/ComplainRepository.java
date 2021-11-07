47419f17b5ae33feeef23a74ec41cb7d51b93cf2
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2021-11-07 10:31:31 +0530

    Get complains by id and orgid

--- src/main/java/ml/ridex/ridexapi/repository/ComplainRepository.java
+++ src/main/java/ml/ridex/ridexapi/repository/ComplainRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ComplainRepository extends MongoRepository<Complain, String> {
     public List<Complain> findByRideRideRequestOrganizationId(String id);
+    public Optional<Complain> findByIdAndRideRideRequestOrganizationId(String id, String orgId);
 }
